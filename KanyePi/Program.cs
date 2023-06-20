2f51f017f899663f2bcceb177832bb48723c3273
Jeremy Tucker <jeremytucker76@gmail.com>
2023-06-19 20:49:32 -0400

    KanyePi

---
+++ KanyePi/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+
+
+
+namespace KanyePi
+{  
+    class Program
+    { 
+        public static string Main(string[] args)
+        {
+            var client = new HttpClient();
+            string kanyeURL = null;
+            var KanyeResponse = client.GetStringAsync(kanyeURL).Result;
+            var kanyeQuote = JObject.Parse(KanyeResponse).GetValue("quote").ToString();
+            return kanyeQuote;
+        }
+    }
+}
+
